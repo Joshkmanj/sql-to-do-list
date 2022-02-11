e1be5dd7df0e71987615f1762d1bdaa769553562
Josh Kralewski <joshkmanj@gmail.com>
2022-02-11 16:54:34 -0600

    Server setup and linked to Pool.js and Task-router.js, client.js setup and linked to jquery

---
+++ server/modules/pool.js
@@ -0,0 +1,22 @@
+const pg = require('pg');
+const Pool = pg.Pool;
+
+const config = {
+    database: 'tasks',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+};
+
+const pool = new Pool(config);
+
+pool.on("connect", () => {
+    console.log('connected to postgreSQL');
+    
+})
+pool.on('error', (error) => {
+    console.log('error connecting to postgreSQL', error);
+});
+
+module.exports = pool;
