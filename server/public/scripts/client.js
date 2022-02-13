3ca33c4fcad7e06bbc5f348aa30b8909d02fc344
Josh Kralewski <joshkmanj@gmail.com>
2022-02-12 18:08:10 -0600

    Client GET route created and linked to server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,38 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('jquery is ready');
+    // On page load/reload, this will retrieve data from the database
+    getTasks();
 }
+
+
+// ------------ < GET/SELECT Routes > ------------------------------------------
+function getTasks() {
+    console.log('Refreshing page data');
+    $.ajax({
+        method: "GET",
+        url: "/tasks"
+    }).then(function(response){
+        console.log('getTasks: Response from server:', response);
+        
+    }).catch(function(error) {
+      console.error('getTasks: No response from server:', error);
+        
+    })
+}
+// ------------ < // END GET/SELECT Routes > -----------------------------------
+
+
+// ------------ < POST/INSERT Routes > -----------------------------------------
+
+// ------------ < // END POST/INSERT Routes > ----------------------------------
+
+
+// ------------ < PUT/UPDATE Routes > ------------------------------------------
+
+// ------------ < // END PUT/UPDATE Routes > -----------------------------------
+
+
+// ------------ < DELETE Routes > ----------------------------------------------
+
+// ------------ < // END DELETE Routes > ---------------------------------------
--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -4,9 +4,27 @@ const router = express.Router();
 // Link to database
 const pool = require('../modules/pool')
 
+// ------------ < GET/SELECT Routes > ------------------------------------------
+router.get('/', (req,res) => {
+    console.log('GET request from client to /tasks:', req.body);
+    res.sendStatus(200)
+})
+// ------------ < // END GET/SELECT Routes > -----------------------------------
 
 
+// ------------ < POST/INSERT Routes > -----------------------------------------
 
+// ------------ < // END POST/INSERT Routes > ----------------------------------
+
+
+// ------------ < PUT/UPDATE Routes > ------------------------------------------
+
+// ------------ < // END PUT/UPDATE Routes > -----------------------------------
+
+
+// ------------ < DELETE Routes > ----------------------------------------------
+
+// ------------ < // END DELETE Routes > ---------------------------------------
 
 
 
