957645a552a7c04a0b9ed7349d040a82094fc9ba
Josh Kralewski <joshkmanj@gmail.com>
2022-02-12 19:06:32 -0600

    GET route now appends data to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,19 +6,34 @@ function readyNow() {
     getTasks();
 }
 
+// ------------ < Display handlers > -------------------------------------------
+function renderToDom(array){
+    console.log('Rendering an array to the DOM');
+    $('#taskOutput').empty()
+    for (let object of array) {
+        $('#taskOutput').append(`
+        <tr data-id=${object.id}>
+            <td>${object.task}</td>
+            <td>${object.priority}</td>
+            <td>${object.status}</td>
+        </tr>`)
+    }
+}
+// ------------ < // END Display handlers > ------------------------------------
+
+
 
 // ------------ < GET/SELECT Routes > ------------------------------------------
 function getTasks() {
-    console.log('Refreshing page data');
+    console.log('Refreshing page data');//<-- Test log to ensure this stage is being reached
     $.ajax({
         method: "GET",
         url: "/tasks"
-    }).then(function(response){
-        console.log('getTasks: Response from server:', response);
-        
-    }).catch(function(error) {
-      console.error('getTasks: No response from server:', error);
-        
+    }).then(function(response){ // If the server responds...
+        console.log('getTasks: Response from server:', response); //<-- logs response contents in console for test purposes
+        renderToDom(response) //<--- Sends the response to be appended to DOM
+    }).catch(function(error) {// In case of no response/error...
+      console.error('getTasks: No response from server:', error);//<-- logs the error
     })
 }
 // ------------ < // END GET/SELECT Routes > -----------------------------------
