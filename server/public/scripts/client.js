7a5599dfb0ea70d45a894966aff024732baaf833
Josh Kralewski <joshkmanj@gmail.com>
2022-02-12 19:31:01 -0600

    Client-side of POST route created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,7 @@ function readyNow() {
     console.log('jquery is ready');
     // On page load/reload, this will retrieve data from the database
     getTasks();
+    $('#input-button').on('click', addTasks)
 }
 
 // ------------ < Display handlers > -------------------------------------------
@@ -21,18 +22,16 @@ function renderToDom(array){
 }
 // ------------ < // END Display handlers > ------------------------------------
 
-
-
 // ------------ < GET/SELECT Routes > ------------------------------------------
 function getTasks() {
-    console.log('Refreshing page data');//<-- Test log to ensure this stage is being reached
+    console.log('Refreshing page data');//<-- Test to ensure this stage is being reached
     $.ajax({
         method: "GET",
         url: "/tasks"
     }).then(function(response){ // If the server responds...
         console.log('getTasks: Response from server:', response); //<-- logs response contents in console for test purposes
         renderToDom(response) //<--- Sends the response to be appended to DOM
-    }).catch(function(error) {// In case of no response/error...
+    }).catch(function(error) { // In case of no response/error...
       console.error('getTasks: No response from server:', error);//<-- logs the error
     })
 }
@@ -40,7 +39,29 @@ function getTasks() {
 
 
 // ------------ < POST/INSERT Routes > -----------------------------------------
+function addTasks(){
+    console.log('Adding new task!'); //<-- Test to ensure this stage is being reached
 
+    let newTaskObject = { // <-- this grabs the data from the input field and packs it in an object
+        task: $('#task-intake').val(),
+        priority: $('#priority-selector').val()
+    }
+    console.log('new task:', newTaskObject);
+    
+
+    $.ajax({
+        method: "POST",
+        url: '/tasks',
+        data:{
+            newTaskObject // This is the object that is sent to the server
+        }
+    }).then(function(response){ // If the server responds...
+        console.log('addTasks: Response from server:', response);//<-- logs response contents in console for test purposes
+        getTasks()
+    }).catch(function(error) { // In case of no response/error...
+        console.error('addTasks: No response from server:', error) //<-- logs the error
+    })
+}
 // ------------ < // END POST/INSERT Routes > ----------------------------------
 
 
